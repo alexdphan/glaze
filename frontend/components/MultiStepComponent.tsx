c7a84d4f30bea84d3a8418aa743bbf5e881f7935
alexdphan <alexphan0515@gmail.com>
2024-05-04 18:50:50 -0400

    feature: show rate limit message (newer)

--- frontend/components/MultiStepComponent.tsx
+++ frontend/components/MultiStepComponent.tsx
@@ -204,8 +204,9 @@ export default function MultiStepComponent() {
                       transition={{ duration: 0.3 }}
                       className="text-response"
                     >
-                      {glaze.startsWith('Rate limit exceeded') ||
-                      glaze.startsWith('Error calling API') ? (
+                      {glaze &&
+                      (glaze.startsWith('Rate limit exceeded') ||
+                        glaze.startsWith('Error calling API')) ? (
                         <p className="text-foreground mt-6">{glaze}</p>
                       ) : (
                         <p className="text-foreground mt-6">{glaze}</p>
