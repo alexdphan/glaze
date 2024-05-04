40440152213885330731343d1289d69a18022679
alexdphan <alexphan0515@gmail.com>
2024-05-04 00:01:08 -0400

    change: make .ico become .png

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -20,7 +20,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <link rel="icon" href="/favicon.ico" sizes="any" />
       <body className={DM.className}>
         <CSPostHogProvider>{children}</CSPostHogProvider>
       </body>
